2c8e27be1a9c673471d4cd728e968b6c891cd1ba
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-09-28 10:09:04 -0400

    Initial Commit

--- events/message.js
+++ events/message.js
@@ -4,21 +4,10 @@ module.exports = async (client, msg) => {
 
     const msgArray = msg.content.split(" ");
     const args = msgArray.slice(1);
-    const cmd = client.commands.get(msgArray[0].slice(1)) || client.commands.find(c => c.alias.includes(msgArray[0].slice(1)));
+    const cmd = client.commands.get(msgArray[0].slice(prefix.length)) || client.commands.find(c => c.alias.includes(msgArray[0].slice(prefix.length)));
 
 
     if (!msg.guild || msg.author.bot) return;
-
-    /*const user = ['placeholder'].get(`${msg.guild.id}-${msg.author.id}`);
-    if (!user.lastMessage) {
-
-        user.lastMessage = true;
-        setTimeout(() => {
-            user.lastMessage = false;
-        },1000*30);
-        user.xp(1000);
-
-    }*/
 
     if (!cmd || !msg.content.startsWith(prefix)) return;
     if (!msg.member.permissions.has(cmd.permission)) return;
