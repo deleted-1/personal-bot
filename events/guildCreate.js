d82b37c9acbb0cdf44f5671f4bf1dfbfe8729ace
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-09-29 09:59:42 -0400

    Leave all servers that isn't my test server or the main server.

---
+++ events/guildCreate.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "guildCreate",
+    run: async (_client, guild) => {
+
+        if (guild.id !== "623752428289785856" && guild.id !== "573988137651404810") guild.leave();
+        
+            
+
+    }
+}
--- events/ready.js
+++ events/ready.js
@@ -20,22 +20,25 @@ module.exports = {
             profiles.prepare("CREATE TABLE profiles (userid TEXT, guildid TEXT, spouse TEXT, cash INTEGER, rep INTEGER, xp INTEGER, daily INTEGER, lastRep INTEGER)").run();
 
         client.guilds.forEach(guild => {
-            guild.members.forEach(member => {
+            if (guild.id !== "623752428289785856" && guild.id !== "573988137651404810") guild.leave();
+            else{
+                guild.members.forEach(member => {
 
-                let m = profiles.prepare("SELECT * FROM profiles WHERE userid = ? AND guildid = ?").get(member.id,guild.id);
-                if (!m)
-                    profiles.prepare("INSERT INTO profiles VALUES(@userid, @guildid, @spouse, @cash, @rep, @xp, @daily, @lastRep)").run({
-                        userid:member.id,
-                        guildid:guild.id,
-                        spouse:'',
-                        cash:0,
-                        rep:0,
-                        xp:0,
-                        daily:0,
-                        lastRep:0
-                    });
-                
-            });
+                    let m = profiles.prepare("SELECT * FROM profiles WHERE userid = ? AND guildid = ?").get(member.id,guild.id);
+                    if (!m)
+                        profiles.prepare("INSERT INTO profiles VALUES(@userid, @guildid, @spouse, @cash, @rep, @xp, @daily, @lastRep)").run({
+                            userid:member.id,
+                            guildid:guild.id,
+                            spouse:'',
+                            cash:0,
+                            rep:0,
+                            xp:0,
+                            daily:0,
+                            lastRep:0
+                        });
+                    
+                });
+            }
         });
     }
 }
