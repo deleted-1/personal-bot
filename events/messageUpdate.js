88a40e0aa70e497f05e093f8c17450b3ef6f8491
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-09-29 10:10:03 -0400

    Logging updated messages

---
+++ events/messageUpdate.js
@@ -0,0 +1,26 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+    name: "messageUpdate",
+    run: async (client, oldMessage, newMessage) => {
+
+        const {guild, author, content:oldContent} = oldMessage;
+        const {content:newContent} = newMessage;
+        if (guild.id == "623752428289785856") {
+            const embed = new RichEmbed()
+                .setAuthor(author.tag, author.avatarURL)
+                .setTitle("Message Delete")
+                .setThumbnail(author.avatarURL)
+                .addField("ID",author.id,true)
+                .addField("Status",author.presence.status,true)
+                .addField("Original Message",oldContent)
+                .addField("Current Message",newContent)
+                .setColor("LIGHT_GREY")
+                .setTimestamp();
+            
+            await client.channels.get("623758761894346752").send(embed); 
+
+        }
+
+    }
+}
